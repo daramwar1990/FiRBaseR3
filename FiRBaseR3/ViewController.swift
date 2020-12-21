44678d666fa85f075304837ef0ca9b0d96058ff0
vinayak <tuptewar7@gmail.com>
2020-12-21 23:18:47 +0530

    Initial Commit

---
+++ FiRBaseR3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FiRBaseR3
+//
+//  Created by Vinayak Balaji Tuptewar on 21/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
